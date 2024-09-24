1c73c4efbad85c15c62bd8e0a644d5fd753e618d
Anastasiia Hurska <nastyagurska@gmail.com>
2024-09-24 18:37:06 +0300

    services section done

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-import './style.css'
+import "./style.css";
+
+const servicesSlider = document.querySelector(".servicesSlider");
+const servicesSliderPrevButton = document.querySelector(
+  ".servicesSliderPrevButton"
+);
+const servicesSliderNextButton = document.querySelector(
+  ".servicesSliderNextButton"
+);
+const servicesSlideAll = document.querySelectorAll(".servicesSlide");
+const servicesSlideCount = document.querySelector(".servicesSlideCount");
+
+const checkServicesSliderButtons = () => {
+  servicesSliderPrevButton.disabled =
+    servicesSlideAll[0].classList.contains("active");
+  servicesSliderNextButton.disabled =
+    servicesSlideAll[servicesSlideAll.length - 1].classList.contains("active");
+};
+
+const handleActiveSlideClass = (element) => {
+  servicesSlideAll.forEach((el) => {
+    el.classList.remove("active");
+  });
+  element.classList.add("active");
+
+  checkServicesSliderButtons();
+};
+
+servicesSliderPrevButton.addEventListener("click", () => {
+  servicesSlider.scrollLeft -= 590;
+  const slideIndex = Math.ceil(servicesSlider.scrollLeft / 590) - 1;
+  servicesSlideCount.textContent = slideIndex + 1;
+  handleActiveSlideClass(servicesSlideAll[slideIndex]);
+});
+
+servicesSliderNextButton.addEventListener("click", () => {
+  servicesSlider.scrollLeft += 590;
+  const slideIndex = Math.ceil(servicesSlider.scrollLeft / 590) + 1;
+  servicesSlideCount.textContent = slideIndex + 1;
+  handleActiveSlideClass(servicesSlideAll[slideIndex]);
+});
